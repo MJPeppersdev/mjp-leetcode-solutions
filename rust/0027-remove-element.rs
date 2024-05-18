d5dcd7eb2f00291d214e9adbfcb840e99e32cdd1
Michael J Peppers <mcgdevfunk824@protonmail.com>
2024-05-17 23:57:47 -0700

    Update 0027-remove-element.rs

--- rust/0027-remove-element.rs
+++ rust/0027-remove-element.rs
@@ -1,15 +1,19 @@
 impl Solution {
-    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-             let mut dup_count = 0;
-
-        for i in 1..nums.len() {
-            if nums[i] == nums[i - 1] {
-                dup_count += 1
-            }
-
-            nums[i - dup_count] = nums[i];
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        while let Some(index) = nums.iter().position(|v| *v == val) {
+            nums.swap_remove(index);
         }
+        nums.len() as i32
+    }
 
-        (nums.len() - dup_count) as i32
+    pub fn remove_element_2(nums: &mut Vec<i32>, val: i32) -> i32 {
+        let mut k = 0;
+        for i in 0..nums.len() {
+            if nums[i] != val {
+                nums[k] = nums[i];
+                k += 1;
+            }
+        }
+        k as i32
     }
 }
