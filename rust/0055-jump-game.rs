ed2f551d7eaf6b6c32aab39dd603af9b8d86339a
Michael J Peppers <mcgdevfunk824@protonmail.com>
2024-05-18 00:12:07 -0700

    Update 0055-jump-game.rs

--- rust/0055-jump-game.rs
+++ rust/0055-jump-game.rs
@@ -1,11 +1,13 @@
-pub fn can_jump(nums: Vec<i32>) -> bool {
-    let mut goal = nums.len() - 1;
+impl Solution {
+    pub fn can_jump(nums: Vec<i32>) -> bool {
+        let mut goal = nums.len() - 1;
 
-    for i in (0..goal).rev() {
-        if i + nums[i] as usize >= goal {
-            goal = i;
+        for i in (0..goal).rev() {
+            if i + nums[i] as usize >= goal {
+                goal = i;
+            }
         }
-    }
 
-    goal == 0
+        goal == 0
+    }
 }
