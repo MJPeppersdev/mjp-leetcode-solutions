97935aebbd815fd8192cddfb24389248d93dc3a1
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-05-17 21:53:22 -0700

    Add files via upload

---
+++ swift/0049-group-anagrams.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+      var ans = [[Int]:[String]]()
+      let a = "a" as Character
+    
+      for s in strs {
+        var count = [Int](repeating: 0, count: 26)
+        for c in Array(s) {
+            count[Int(c.asciiValue! - a.asciiValue!)] += 1
+        }
+        ans[count, default: []].append(s)
+      }
+    return Array(ans.values)
+  }
+}
