715aad17d3b610f56209b3e14a3f064e47bc6259
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-05-19 01:18:34 -0700

    Add files via upload

---
+++ java/0074-search-a-2d-matrix.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0, j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+		 return true;
+	    } else if (matrix[i][j] > target) {
+		 j--;
+	    } else {
+		 i++;
+	    }
+        }
+        return false;
+    }
+
+    //    Time: O(log(mn)) | Space: O(1)
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        if(matrix.length == 0) return false;
+
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int low = 0;
+        int high = rows * columns;
+
+        while(low < high) {
+            int mid = (low+high)/2;
+
+            if(matrix[mid/columns][mid%columns] == target) {
+                return true;
+            } else if (matrix[mid/columns][mid%columns] < target) {
+                low = mid+1;
+            } else {
+                high = mid;
+            }
+        }
+        return false;
+    }
+}
---
+++ java/0083-remove-duplicates-from-sorted-list.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode p = null;
+        ListNode q = null;
+        ListNode r = head;
+        while (r != null) {
+            if (q != null && q.val == r.val) {
+                r = r.next;
+                q.next = r;
+            } else {
+                p = q;
+                q = r;
+                r = r.next;
+            }
+        }
+        return head;
+    }
+}
