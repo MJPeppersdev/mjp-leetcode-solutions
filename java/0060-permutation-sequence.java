5d307f3629c5dcbdfa2bee84b267b9832d437939
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-05-18 23:07:34 -0700

    Add files via upload

---
+++ java/0060-permutation-sequence.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    // Time complexity O(N^2) becuase of remove method
+    public String getPermutation(int n, int k) {
+        StringBuilder kthPerm = new StringBuilder();
+        int fact = 1;
+        //this list will contain all the values from 1 to n for reference
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i < n; i++) {
+            fact = fact * i;
+            list.add(i);
+        }
+        list.add(n);
+        k--;
+        while (true) {
+            kthPerm.append(list.get(k / fact));
+            list.remove(k / fact);
+            if (list.size() == 0) break;
+            k = k % fact;
+            fact = fact / (list.size());
+        }
+        return kthPerm.toString();
+    }
