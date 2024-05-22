663e57300cd2921aa0bd610efe4e9a90138d0925
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-05-22 14:20:25 -0700

    Add files via upload

---
+++ swift/0380-insert-delete-getrandom-01.swift
@@ -0,0 +1,29 @@
+class RandomizedSet {
+    var random = Set<Int>()
+
+    init() {
+    
+    }
+    
+    func insert(_ val: Int) -> Bool {
+        if !random.contains(val){
+            random.insert(val)
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func remove(_ val: Int) -> Bool {
+        if random.contains(val) {
+            random.remove(val)
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func getRandom() -> Int {
+        return random.randomElement()!
+    }
+}
---
+++ swift/0392-is-subsequence.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func isSubsequence(_ s: String, _ t: String) -> Bool {
+        if s.isEmpty {
+            return true
+        }
+
+        var s = Array(s)
+        var i = 0
+
+        for c in t {
+            if c == s[i] {
+                i += 1
+                if i == s.count {
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+}
