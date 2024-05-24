b5cd0e6f9df3ef0d9964bbc1dce80ffcb49a56df
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-05-24 01:11:55 -0700

    Update 1220-Count-Vowels-Permutation.java

--- java/1220-Count-Vowels-Permutation.java
+++ java/1220-Count-Vowels-Permutation.java
@@ -1,9 +1,3 @@
-/*          Bottom-Up Approach
------------------------------------------
-    Time Complexity : O(n)
-    Space Complexity : O(n)
-----------------------------------------*/
-
 class Solution {
     int MOD = (int) 1e9+7; 
 
@@ -34,52 +28,4 @@ class Solution {
     }
 }
 
-/*          Top Down Approach
------------------------------------------
-    Time Complexity : O(n)
-    Space Complexity : O(n)
-----------------------------------------*/
 
-class Solution {
-    HashMap<String, Integer> memo = new HashMap<>();
-    int MOD = (int) 1e9+7; 
-
-    public int countVowelPermutation(int n) {
-        long ans = 0;
-        ans = (ans + dfs('a', n, 1)) % MOD;
-        ans = (ans + dfs('e', n, 1)) % MOD;
-        ans = (ans + dfs('i', n, 1)) % MOD;
-        ans = (ans + dfs('o', n, 1)) % MOD;
-        ans = (ans + dfs('u', n, 1)) % MOD;
-        return (int)ans;
-    }
-
-    private int dfs(char c, int n, int l){
-        if(l == n)
-            return 1;
-        
-        String key = c + "_" + l;
-        if (memo.containsKey(key)) return memo.get(key);
-
-        long res = 0;
-        if(c == 'a') {
-            res = dfs('e', n, l+1);
-        } else if(c == 'e') {
-            res = (res + dfs('a', n, l+1)) % MOD;
-            res = (res + dfs('i', n, l+1)) % MOD;
-        } else if(c == 'i') {
-            res = (res + dfs('a', n, l+1)) % MOD;
-            res = (res + dfs('e', n, l+1)) % MOD;
-            res = (res + dfs('o', n, l+1)) % MOD;
-            res = (res + dfs('u', n, l+1)) % MOD;
-        } else if(c == 'o') {
-            res = (res + dfs('i', n, l+1)) % MOD;
-            res = (res + dfs('u', n, l+1)) % MOD;
-        } else {
-            res = dfs('a', n, l+1);
-        }
-
-        memo.put(key, (int)(res % MOD));
-        return (int)(res % MOD);    
-    }
-}
