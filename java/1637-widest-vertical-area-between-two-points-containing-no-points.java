821df7a2bf1096463d8ad768be28bd7d2b0309b7
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-05-24 11:41:15 -0700

    Add files via upload

---
+++ java/1637-widest-vertical-area-between-two-points-containing-no-points.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, (p1, p2) -> p1[0] - p2[0]);
+
+        int res = 0;
+        for(int i = 1; i < points.length; i++){
+            res = Math.max(res, points[i][0] - points[i-1][0]);
+        }
+        return res;
+    }
+}
