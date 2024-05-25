9c63f03c79927aeef9ffbb3583ad26c682917262
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-05-25 12:05:16 -0700

    Create 0946-validate-stack-sequences.cpp

---
+++ c++/0946-validate-stack-sequences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> stk;
+        int i = 0;
+        for (int num : pushed) {
+            stk.push(num);
+            while (!stk.empty() && stk.top() == popped[i]) {
+                stk.pop();
+                ++i;
+            }
+        }
+        return stk.empty();
+    }
+};
+
