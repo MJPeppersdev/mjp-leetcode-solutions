020f336bc426f812ad44536484e6ce328864c22d
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-06-02 17:40:24 -0700

    Create 0106-construct-binary-tree-from-inorder-and-postorder-traversal.kt

---
+++ kotlin/0106-construct-binary-tree-from-inorder-and-postorder-traversal.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun buildTree(inorder: IntArray, postorder: IntArray): TreeNode? {
+        val inOrdIdx = HashMap<Int, Int>().apply{
+            for((i,v) in inorder.withIndex())
+                this[v] = i
+        }
+
+        var pstOrdIdx = postorder.lastIndex
+
+        fun helper(left: Int, right: Int): TreeNode? {
+            if(left > right)
+                return null
+        
+            val root = TreeNode(postorder[pstOrdIdx])
+            pstOrdIdx--
+
+            val index = inOrdIdx[root.value]!!
+            root.right = helper(index + 1, right)
+            root.left = helper(left, index - 1)
+            return root
+        }
+        
+        return helper(0, postorder.lastIndex)
+    }
+
+    val TreeNode.value get()= this.`val`
+}
