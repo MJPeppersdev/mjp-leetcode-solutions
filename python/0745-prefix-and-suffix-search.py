870562799c4d8915988d4e55b28c0575a76249f1
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-06-05 11:06:15 -0700

    Add files via upload

---
+++ python/0745-prefix-and-suffix-search.py
@@ -0,0 +1,13 @@
+class WordFilter:
+    def __init__(self, words: List[str]):
+        self.d = {}
+        for k, w in enumerate(words):
+            n = len(w)
+            for i in range(n + 1):
+                a = w[:i]
+                for j in range(n + 1):
+                    b = w[j:]
+                    self.d[(a, b)] = k
+
+    def f(self, pref: str, suff: str) -> int:
+        return self.d.get((pref, suff), -1)
