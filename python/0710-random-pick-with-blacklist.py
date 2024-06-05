4d8d7b7d13f4f2d60c0290e571ee64cecf4ded9e
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-06-05 10:56:59 -0700

    Create 0710-random-pick-with-blacklist.py

---
+++ python/0710-random-pick-with-blacklist.py
@@ -0,0 +1,16 @@
+class Solution:
+    def __init__(self, n: int, blacklist: List[int]):
+        self.k = n - len(blacklist)
+        self.d = {}
+        i = self.k
+        black = set(blacklist)
+        for b in blacklist:
+            if b < self.k:
+                while i in black:
+                    i += 1
+                self.d[b] = i
+                i += 1
+
+    def pick(self) -> int:
+        x = randrange(self.k)
+        return self.d.get(x, x)
