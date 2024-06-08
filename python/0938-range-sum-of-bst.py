92a6de853fe0fb1ed2f33795b61b82eeb8e54a25
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-06-08 01:47:22 -0700

    Add files via upload

---
+++ python/0938-range-sum-of-bst.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        def dfs(root: Optional[TreeNode]) -> int:
+            if root is None:
+                return 0
+            x = root.val
+            ans = x if low <= x <= high else 0
+            if x > low:
+                ans += dfs(root.left)
+            if x < high:
+                ans += dfs(root.right)
+            return ans
+
+        return dfs(root)
