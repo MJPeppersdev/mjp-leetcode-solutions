9c0e85a094b0ae47b989eee7519a7d1c6315bbd2
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-06-11 11:34:52 -0700

    Delete python directory

--- python/0215-kth-largest-element-in-an-array.py
+++
@@ -1,4 +0,0 @@
-class Solution1:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        return nums[len(nums) - k]
--- python/0965-univalued-binary-tree.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isUnivalTree(self, root: TreeNode) -> bool:
-        def dfs(node):
-            if node is None:
-                return True
-            return node.val == root.val and dfs(node.left) and dfs(node.right)
-
-        return dfs(root)
