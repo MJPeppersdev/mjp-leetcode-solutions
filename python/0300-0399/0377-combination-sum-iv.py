608e37efda8e62369e83b1843c65b02cdf5a8397
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-06-11 12:00:24 -0700

    Add files via upload

---
+++ python/0300-0399/0377-combination-sum-iv.py
@@ -0,0 +1,10 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        cache = {0: 1}
+
+        for total in range(1, target + 1):
+            cache[total] = 0
+            for n in nums:
+                cache[total] += cache.get(total - n, 0)
+        return cache[target]
+
