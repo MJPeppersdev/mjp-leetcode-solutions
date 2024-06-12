d42aca2f3749025a288a8c3d1af953a3671823d5
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-06-11 23:28:25 -0700

    Add files via upload

---
+++ python/1300-1399/1372-longest-zigzag-path-in-a-binary-tree.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestZigZag(self, root: TreeNode) -> int:
+        def dfs(root, l, r):
+            if root is None:
+                return
+            nonlocal ans
+            ans = max(ans, l, r)
+            dfs(root.left, r + 1, 0)
+            dfs(root.right, 0, l + 1)
+
+        ans = 0
+        dfs(root, 0, 0)
+        return ans
