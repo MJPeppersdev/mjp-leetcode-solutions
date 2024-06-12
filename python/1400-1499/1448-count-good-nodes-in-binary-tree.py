f6b6640fdfb0546e1c7ef3822c2380360e0ba13c
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-06-12 12:21:44 -0700

    Add files via upload

---
+++ python/1400-1499/1448-count-good-nodes-in-binary-tree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(root: TreeNode, mx: int):
+            if root is None:
+                return
+            nonlocal ans
+            if mx <= root.val:
+                ans += 1
+                mx = root.val
+            dfs(root.left, mx)
+            dfs(root.right, mx)
+
+        ans = 0
+        dfs(root, -1000000)
+        return ans
