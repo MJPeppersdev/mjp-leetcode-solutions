61e88d62413d5a0cd9292899e005a15575bb869b
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-06-18 22:35:19 -0700

    Create 1831-maximum-transaction-each-day.sql

---
+++ sql/1831-maximum-transaction-each-day.sql
@@ -0,0 +1,14 @@
+WITH
+    T AS (
+        SELECT
+            transaction_id,
+            RANK() OVER (
+                PARTITION BY DAY(day)
+                ORDER BY amount DESC
+            ) AS rk
+        FROM Transactions
+    )
+SELECT transaction_id
+FROM T
+WHERE rk = 1
+ORDER BY 1;
