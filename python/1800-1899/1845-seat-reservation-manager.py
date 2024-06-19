bc632c6dda4fe7d893fd23343067dc1aae88a9c7
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-06-18 22:36:39 -0700

    Add files via upload

---
+++ python/1800-1899/1845-seat-reservation-manager.py
@@ -0,0 +1,10 @@
+class SeatManager:
+    def __init__(self, n: int):
+        self.q = list(range(1, n + 1))
+        heapify(self.q)
+
+    def reserve(self) -> int:
+        return heappop(self.q)
+
+    def unreserve(self, seatNumber: int) -> None:
+        heappush(self.q, seatNumber)
