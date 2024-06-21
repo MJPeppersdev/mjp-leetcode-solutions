208aed3490cf8b5f3d6e0362c93c4265f4b72cc5
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-06-21 13:48:17 -0700

    Create 2041-accepted-candidates-from-the-interviews.sql

---
+++ sql/2001-3000/2041-accepted-candidates-from-the-interviews.sql
@@ -0,0 +1,7 @@
+SELECT candidate_id
+FROM
+    Candidates AS c
+    LEFT JOIN Rounds AS r ON c.interview_id = r.interview_id
+WHERE years_of_exp >= 2
+GROUP BY c.interview_id
+HAVING SUM(score) > 15;
