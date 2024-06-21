39583f982034fa6472d4d951fe08fdddf1dc6b28
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-06-21 13:49:31 -0700

    Add files via upload

---
+++ python/2000-2099/2034-stock-price-fluctuation.py
@@ -0,0 +1,24 @@
+from sortedcontainers import SortedList
+
+
+class StockPrice:
+    def __init__(self):
+        self.d = {}
+        self.ls = SortedList()
+        self.last = 0
+
+    def update(self, timestamp: int, price: int) -> None:
+        if timestamp in self.d:
+            self.ls.remove(self.d[timestamp])
+        self.d[timestamp] = price
+        self.ls.add(price)
+        self.last = max(self.last, timestamp)
+
+    def current(self) -> int:
+        return self.d[self.last]
+
+    def maximum(self) -> int:
+        return self.ls[-1]
+
+    def minimum(self) -> int:
+        return self.ls[0]
