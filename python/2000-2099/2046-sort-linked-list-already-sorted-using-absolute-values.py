4600e9eefb840265340a8419a14432e8e8ef5cd0
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-06-21 13:50:11 -0700

    Add files via upload

---
+++ python/2000-2099/2046-sort-linked-list-already-sorted-using-absolute-values.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortLinkedList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = head, head.next
+        while curr:
+            if curr.val < 0:
+                t = curr.next
+                prev.next = t
+                curr.next = head
+                head = curr
+                curr = t
+            else:
+                prev, curr = curr, curr.next
+        return head
