735bf8e3afffb93c28da67f102258c8680530984
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-06-24 21:52:40 -0700

    Add files via upload

---
+++ python/2100-2199/2130-maximum-twin-sum-of-a-linked-list.py
@@ -0,0 +1,8 @@
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        s = []
+        while head:
+            s.append(head.val)
+            head = head.next
+        n = len(s)
+        return max(s[i] + s[-(i + 1)] for i in range(n >> 1))
