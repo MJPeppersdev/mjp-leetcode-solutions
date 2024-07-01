86df0d3749429b5df9289443d107e800ee8c215e
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-07-01 15:25:35 -0700

    Create 2324-product-sales-analysis-iv.sql

---
+++ sql/2001-3000/2324-product-sales-analysis-iv.sql
@@ -0,0 +1,17 @@
+WITH
+    T AS (
+        SELECT
+            user_id,
+            product_id,
+            RANK() OVER (
+                PARTITION BY user_id
+                ORDER BY SUM(quantity * price) DESC
+            ) AS rk
+        FROM
+            Sales
+            JOIN Product USING (product_id)
+        GROUP BY 1, 2
+    )
+SELECT user_id, product_id
+FROM T
+WHERE rk = 1;
