a8ef74b2366e5ed1c776e642caf1dfa984a2b220
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-07-14 00:02:41 -0700

    Add files via upload

---
+++ typescript/2700-2799/2723-add-two-promises.ts
@@ -0,0 +1,13 @@
+type P = Promise<number>
+
+async function addTwoPromises(
+    promise1: Promise<number>,
+    promise2: Promise<number>,
+): Promise<number> {
+    return (await promise1) + (await promise2);
+}
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
