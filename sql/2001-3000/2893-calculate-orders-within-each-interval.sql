ba42f6608220d906d8d0a956097ab66dc83e5d9b
Michael J Peppers <mjpeppersdev@protonmail.com>
2024-07-26 00:28:48 -0700

    Create 2893-calculate-orders-within-each-interval.sql

---
+++ sql/2001-3000/2893-calculate-orders-within-each-interval.sql
@@ -0,0 +1,13 @@
+WITH
+    T AS (
+        SELECT
+            minute,
+            SUM(order_count) OVER (
+                ORDER BY minute
+                ROWS 5 PRECEDING
+            ) AS total_orders
+        FROM Orders
+    )
+SELECT minute / 6 AS interval_no, total_orders
+FROM T
+WHERE minute % 6 = 0;
